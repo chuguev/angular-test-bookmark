d6c5e92d46b01ca47b0fd07f772f4946fa67daec
m.chuguev <m.chuguev@watcom.ru>
2020-06-07 01:21:58 +0300

    Исправлены конфликты с линтовщиком

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,8 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'angular-test-bookmark app is running!'
-    );
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-test-bookmark app is running!');
   });
 });
